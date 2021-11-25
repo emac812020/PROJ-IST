0a018622f858c9a996c67ffeb3a6c1cc8ea79e36
emac812020 <85925250+emac812020@users.noreply.github.com>
2021-11-25 10:37:59 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -53,16 +53,16 @@ resource "intersight_adapter_config_policy" "sds-adapter-config-policy" {
 
   
 #------------- ADDING -----   
-  
- resource "intersight_vnic_eth_qos_policy" "sds-ethernet-qos-policy" {
-   name           = "sds-ethernet-qos-policy"
-   description    = "Ethernet quality of service for SDS"
-   mtu            = 9000
-   rate_limit     = 0
-   cos            = 0
-   trust_host_cos = false
-   organization {
-     object_type = "organization.Organization"
-     moid        = module.intersight-moids.organization_moid
-   }
-}
+#  
+# resource "intersight_vnic_eth_qos_policy" "sds-ethernet-qos-policy" {
+#   name           = "sds-ethernet-qos-policy"
+#   description    = "Ethernet quality of service for SDS"
+#   mtu            = 9000
+#   rate_limit     = 0
+#   cos            = 0
+#   trust_host_cos = false
+#   organization {
+#     object_type = "organization.Organization"
+#     moid        = module.intersight-moids.organization_moid
+#   }
+#}
